cb1aff90b8b46eba9a065a1ca613ef614d6e978d
Todd Bernson <todd@bernsonfamily.com>
2025-02-03 13:06:30 -0500

    deploying cobol_0

--- docker/cobol_0/run.sh
+++ docker/cobol_0/run.sh
@@ -2,11 +2,6 @@
 
 set -x
 
-if [ -z "$INPUT_FILE" ] || [ -z "$OUTPUT_FILE" ]; then
-  echo "Error: INPUT_FILE and OUTPUT_FILE environment variables must be set."
-  exit 1
-fi
-
 cobc -x -free TransformCSV.cbl -o TransformCSV
 
 ./TransformCSV
